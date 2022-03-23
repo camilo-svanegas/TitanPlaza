97385abc986742377542b95d701937dfc6dcf903
isumesh <63849019+isumesh@users.noreply.github.com>
2022-03-23 20:09:48 +0530

    Edit: bug fix

--- App/Components/StoresContainer.js
+++ App/Components/StoresContainer.js
@@ -1,66 +1,69 @@
 'use strict';
 
-import React, { Component } from 'react'; 
+import React, {Component} from 'react';
 import {PropTypes} from 'prop-types';
 
 import {
   PickerIOS,
   ScrollView,
-  Text, 
-  TouchableOpacity, 
+  Text,
+  TouchableOpacity,
   View,
   StyleSheet,
   ViewPropTypes,
-  Alert
+  Alert,
 } from 'react-native';
 
-import {
-  debugMe
-} from '../Utilities/Messaging'
+import {debugMe} from '../Utilities/Messaging';
 
-import ScrollableTabView, {DefaultTabBar, } from 'react-native-scrollable-tab-view';
+import ScrollableTabView, {
+  DefaultTabBar,
+} from '../Components/react-native-scrollable-tab-view';
 
 const Stores = require('./Stores');
 const StoresFavorites = require('./StoresFavorites');
 
 import analytics from '@react-native-firebase/analytics';
 
-import { layout, tabs  } from '../Styles/TitanPlaza/Global';
+import {layout, tabs} from '../Styles/TitanPlaza/Global';
 
-export default class StoresContainer extends Component{
-
-  static propTypes = { 
-    //navigator: PropTypes.object.isRequired, 
+export default class StoresContainer extends Component {
+  static propTypes = {
+    //navigator: PropTypes.object.isRequired,
     style: ViewPropTypes.style,
-  }; 
+  };
 
-
-  constructor(props, context) { 
-
-    super(props, context); 
+  constructor(props, context) {
+    super(props, context);
 
     this.navigator = this.props.navigation;
     const {state} = this.navigator;
     //if (state.params) {
-      this.sesion = this.props.route.params["sesion"]? this.props.route.params["sesion"]: null;
-      this.initialPage = this.props.route.params["initialPage"]? this.props.route.params["initialPage"]: 0;
-      this.gotoStore = this.props.route.params["gotoStore"]? this.props.route.params["gotoStore"]: null;
+    this.sesion = this.props.route.params.sesion
+      ? this.props.route.params.sesion
+      : null;
+    this.initialPage = this.props.route.params.initialPage
+      ? this.props.route.params.initialPage
+      : 0;
+    this.gotoStore = this.props.route.params.gotoStore
+      ? this.props.route.params.gotoStore
+      : null;
     //}
 
-    this.state={
-      ranNum:0,
-      tabsChange:0,
+    this.state = {
+      ranNum: 0,
+      tabsChange: 0,
       id_user: this.sesion.id_user,
       token: this.sesion.token,
-    }
+    };
 
     //this._randomNumber = this._randomNumber.bind(this);
   }
 
-  renderLoadingView(){
-    return(
+  renderLoadingView() {
+    return (
       <View style={styles.container}>
-        <Text> Cargando Almacenes ... </Text>  
+        <Text> Cargando Almacenes ... </Text>
       </View>
     );
   }
@@ -73,64 +76,60 @@ export default class StoresContainer extends Component{
     //}else{
     //  this.setState({ranNum:ra, tabsChange:-1});
     //}
-    this.setState({tabsChange:tab.i});
-  }
+    this.setState({tabsChange: tab.i});
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this._registerAnalytics(this.initialPage);
   }
 
-  _registerAnalytics = async (tab) =>{
-
-    if (tab==0){
-       await analytics().logScreenView({
-            screen_name: "Stores",
-            screen_class: "Stores",
-          });
-
-    }else if (tab==1){
-       await analytics().logScreenView({
-            screen_name: "Favoritos",
-            screen_class: "Favoritos",
-          });
-
+  _registerAnalytics = async (tab) => {
+    if (tab == 0) {
+      await analytics().logScreenView({
+        screen_name: 'Stores',
+        screen_class: 'Stores',
+      });
+    } else if (tab == 1) {
+      await analytics().logScreenView({
+        screen_name: 'Favoritos',
+        screen_class: 'Favoritos',
+      });
     }
+  };
 
-  } 
-
-  render(){
-    return(
+  render() {
+    return (
       <ScrollableTabView
         renderTabBar={() => <DefaultTabBar />}
         tabBarUnderlineStyle={tabs.tabIndicator}
         tabBarBackgroundColor="#FAFAFA"
         tabBarActiveTextColor="#903392"
-        tabBarInactiveTextColor='#292C33'
-        tabBarTextStyle={tabs.tabLabel} 
-        tabBarPosition='top' 
-        onChangeTab={async (tab)=>{this._randomNumber(tab); this._registerAnalytics(tab.i) }}
-        initialPage={this.initialPage}
-      >
-        <View 
-          tabLabel="Todos" 
-          style={layout.mainContainer}>
-          <Stores navigator={this.navigator} 
-          tabsChange={this.state.tabsChange} 
-          sesion={this.sesion}
-          gotoStore={this.gotoStore} />
+        tabBarInactiveTextColor="#292C33"
+        tabBarTextStyle={tabs.tabLabel}
+        tabBarPosition="top"
+        onChangeTab={async (tab) => {
+          this._randomNumber(tab);
+          this._registerAnalytics(tab.i);
+        }}
+        initialPage={this.initialPage}>
+        <View tabLabel="Todos" style={layout.mainContainer}>
+          <Stores
+            navigator={this.navigator}
+            tabsChange={this.state.tabsChange}
+            sesion={this.sesion}
+            gotoStore={this.gotoStore}
+          />
         </View>
 
-        <View 
-          tabLabel="Favoritos"
-          style={layout.mainContainer}>
-          <StoresFavorites navigator={this.navigator}
-          tabsChange={this.state.tabsChange}
-          sesion={this.sesion} />
+        <View tabLabel="Favoritos" style={layout.mainContainer}>
+          <StoresFavorites
+            navigator={this.navigator}
+            tabsChange={this.state.tabsChange}
+            sesion={this.sesion}
+          />
         </View>
-
       </ScrollableTabView>
     );
-  
   }
 }
 
